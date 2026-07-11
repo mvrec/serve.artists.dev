6cf5220f3fd69876e23333878dac473a49bfdb4e
Mix Vibe Rec - dev <101111405+mvrec@users.noreply.github.com>
2026-07-11 22:35:35 +0530

    u

--- components/artists-page/artist-page-config.v1.0.1.7-4.js
+++ components/artists-page/artist-page-config.v1.0.1.7-4.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const vToggle = $("#v-toggle");
   if (vToggle) {
     vToggle.addEventListener("click", () => {
-      showToast("Verified By Mix Vibe Records");
+      showToast("Verified By Mix Vibe Records™");
     });
   }
 
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
     <div class="relative w-full h-full transition-transform duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
         <!-- Front Side -->
         <div class="absolute inset-0 [backface-visibility:hidden]">
-            <img src="${pImageSrc}" alt="${pImageAlt}" class="w-full h-full rounded-2xl object-cover artist-pick shadow-xl">
+            <img src="${pImageSrc}" alt="${pImageAlt}" class="w-full h-full rounded-2xl object-cover shadow-xl">
             <div class="verified absolute bottom-2 right-2 p-1 rounded-full shadow-lg">
                 <img src="https://cdn.jsdelivr.net/gh/mvrec/files.mvr.dev@master/img/svg/vfyd.svg" alt="Verified Badge" class="w-7 h-7">
             </div>
